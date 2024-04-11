1d8d7d5f66a6fd5f521b689176b19b013d350031
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2024-04-11 23:25:33 +0200

    Update scripting.js

--- pages/scripting.js
+++ pages/scripting.js
@@ -124,7 +124,7 @@ const Scripting = () =>
                                             <p className="text-center text-white">{description}</p>
                                             <div className="text-center">
                                             <a className="btn btn-primary border button-card" href ={github}>Code</a>
-                                            <a className="btn btn-info border button-card" href ={url}>Code</a>
+                                            <a className="btn btn-info border button-card" href ={url}>Demo</a>
                                             </div>
                                            
                                         </div>
